c6479c318c51b1a0783eaeefa96ce8d42d865a51
Bcypher01 <olumidebabayemi1@gmail.com>
2023-09-22 00:42:50 +0100

    image size consistency

--- src/components/Image.js
+++ src/components/Image.js
@@ -5,9 +5,9 @@ export const Image = forwardRef(
     const inlineStyles = {
       opacity: faded ? "0.2" : "1",
       transformOrigin: "0 0",
-      height: index === 0 ? 410 : 200,
-      gridRowStart: index === 0 ? "span 2" : null,
-      gridColumnStart: index === 0 ? "span 2" : null,
+      height: 400,
+      gridRowStart: "span 2",
+      gridColumnStart: "span 2",
       backgroundImage: `url("${url}")`,
       backgroundSize: "cover",
       backgroundPosition: "center",
